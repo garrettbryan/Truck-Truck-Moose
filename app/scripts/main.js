daa90031c2064471f2fdda916e8024f8f89865a0
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-03-11 11:54:16 -0400

    add knockoutjs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 function initialize() {
   var mapOptions = {
     center: { lat: 35.619075, lng: -78.638199},
-    zoom: 21
+    zoom: 21,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
