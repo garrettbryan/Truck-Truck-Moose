fe151d431f3a7f17254e651a2f09b2d2c006c455
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-11-29 23:05:21 -0500

    feat: Add 10 food trucks to the model and images.
    Eventually the fake food trucks will be replaced by real food trucks.
    Each food truck has an associated array of times and locations.

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -3,4 +3,256 @@ var aboutMy = {
   searches: [],
   markers: [],
   weather: {},
+  foodTrucks: [
+    {
+      name: "Mammoth Meats",
+      description: "Grass fed cows cooked up caveman style.",
+      marker: "images/Woolly_Mammoth.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "The Big Cuban",
+      description: "Authentic cuban sandwiches now that the embargo is lifted.!",
+      marker: "images/SandwichSU.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "The Icee Snowman",
+      description: "Snocones! Try the high fructose corn syrup flavor!",
+      marker: "images/Snowman_SU.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "Tail of the Whale",
+      description: "Sushi from a food truck - awesome!",
+      marker: "images/Blue_Whale.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "Cicada Poppers",
+      description: "Awesome crunch",
+      marker: "images/Cicada.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "The Sailor's Cup",
+      description: "Premium Coffee from around the world",
+      marker: "images/Coffee.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "The Hole Enchilada",
+      description: "Fusion Gourmet Donuts",
+      marker: "images/Doughnut.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "Boomerang Hut",
+      description: "Kangaroo Steaks!",
+      marker: "images/Kangaroo.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "Overdone",
+      description: "Outrageously ostentatious cupcakes",
+      marker: "images/Cupcake.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    },
+    {
+      name: "The Potato Pup",
+      description: "Hotdogs topped with crunchy potato chips",
+      marker: "images/Hot_Dog.png",
+      locTime: [
+        {
+          t0: "12:00pm",
+          t1: "1:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "6:00pm",
+          t1: "7:00pm",
+          x: 100,
+          Y: 100
+        },
+        {
+          t0: "10:00pm",
+          t1: "11:00pm",
+          x: 100,
+          Y: 100
+        }
+      ]
+    }
+  ]
 }
