16e92a84106a2b6085154aed2afac3b294adaf71
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-01 00:37:52 -0500

    feat: learn google autocomplete
    work throught the example on:https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
    https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -138,7 +138,121 @@ function initialize() {
         });
 
 
-        autocomplete = new google.maps.places.AutocompleteService();
+        /* incorporate google auto complete
+        https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
+        */
+
+        var map, places, infoWindow;
+        var markers = [];
+        var autocomplete;
+        var countryRestrict = {'country': 'us'};
+
+        //autocomplete = new google.maps.places.AutocompleteService();
+        // Create the autocomplete object and associate it with the UI input control.
+        // Restrict the search to the default country, and to place type "cities".
+        autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */ (
+          document.getElementById('search-bar')), {
+          types: ['(cities)'],
+          componentRestrictions: countryRestrict
+          });
+        places = new google.maps.places.PlacesService(map);
+
+        //autocomplete.addListener('place_changed', onPlaceChanged);
+
+        // Add a DOM event listener to react when the user selects a country.
+        //document.getElementById('country').addEventListener(
+        //'change', setAutocompleteCountry);
+
+        // Search for hotels in the selected city, within the viewport of the map.
+        function search() {
+          var search = {
+            bounds: map.getBounds(),
+            types: ['lodging']
+          };
+
+          places.nearbySearch(search, function(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+              clearResults();
+              clearMarkers();
+              // Create a marker for each hotel found, and
+              // assign a letter of the alphabetic to each marker icon.
+              for (var i = 0; i < results.length; i++) {
+                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + i);
+                var markerIcon = MARKER_PATH + markerLetter + '.png';
+                // Use marker animation to drop the icons incrementally on the map.
+                markers[i] = new google.maps.Marker({
+                  position: results[i].geometry.location,
+                  animation: google.maps.Animation.DROP,
+                  icon: markerIcon
+                });
+                // If the user clicks a hotel marker, show the details of that hotel
+                // in an info window.
+                markers[i].placeResult = results[i];
+                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+                setTimeout(dropMarker(i), i * 100);
+                addResult(results[i], i);
+              }
+            }
+          });
+        }
+
+        function clearMarkers() {
+          for (var i = 0; i < markers.length; i++) {
+            if (markers[i]) {
+              markers[i].setMap(null);
+            }
+          }
+          markers = [];
+        }
+
+        function dropMarker(i) {
+          return function() {
+            markers[i].setMap(map);
+          };
+        }
+
+
+        function addResult(result, i) {
+          var results = document.getElementById('results');
+          var markerLetter = String.fromCharCode('A'.charCodeAt(0) + i);
+          var markerIcon = MARKER_PATH + markerLetter + '.png';
+
+          var tr = document.createElement('tr');
+          tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
+          tr.onclick = function() {
+            google.maps.event.trigger(markers[i], 'click');
+          };
+
+          var iconTd = document.createElement('td');
+          var nameTd = document.createElement('td');
+          var icon = document.createElement('img');
+          icon.src = markerIcon;
+          icon.setAttribute('class', 'placeIcon');
+          icon.setAttribute('className', 'placeIcon');
+          var name = document.createTextNode(result.name);
+          iconTd.appendChild(icon);
+          nameTd.appendChild(name);
+          tr.appendChild(iconTd);
+          tr.appendChild(nameTd);
+          results.appendChild(tr);
+        }
+
+        function clearResults() {
+          var results = document.getElementById('results');
+          while (results.childNodes[0]) {
+            results.removeChild(results.childNodes[0]);
+          }
+        }
+
+
+
+
+
+
+
+
+
 
       },
       /*failure callback returns err
