3b63dbb8d91179e5be69abdd149f6eed9c22ed76
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-02 00:05:29 -0500

    feat: update marker images
    use smaller resized images for the map markers.

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -7,61 +7,61 @@ var aboutMy = {
     {
       name: "Mammoth Meats",
       description: "Grass fed cows cooked up caveman style.",
-      marker: "images/Woolly_Mammoth.png",
+      marker: "images/resize_Woolly_Mammoth.png",
       locTime: []
     },
     {
       name: "The Big Cuban",
       description: "Authentic cuban sandwiches now that the embargo is lifted.!",
-      marker: "images/SandwichSU.png",
+      marker: "images/resize_SandwichSU.png",
       locTime: []
     },
     {
       name: "The Icee Snowman",
       description: "Snocones! Try the high fructose corn syrup flavor!",
-      marker: "images/Snowman_SU.png",
+      marker: "images/resize_Snowman_SU.png",
       locTime: []
     },
     {
       name: "Tail of the Whale",
       description: "Sushi from a food truck - awesome!",
-      marker: "images/Blue_Whale.png",
+      marker: "images/resize_Blue_Whale.png",
       locTime: []
     },
     {
       name: "Cicada Poppers",
       description: "Awesome crunch",
-      marker: "images/Cicada.png",
+      marker: "images/resize_Cicada.png",
       locTime: []
     },
     {
       name: "The Sailor's Cup",
       description: "Premium Coffee from around the world",
-      marker: "images/Coffee.png",
+      marker: "images/resize_Coffee.png",
       locTime: []
     },
     {
       name: "The Hole Enchilada",
       description: "Fusion Gourmet Donuts",
-      marker: "images/Doughnut.png",
+      marker: "images/resize_Doughnut.png",
       locTime: []
     },
     {
       name: "Boomerang Hut",
       description: "Kangaroo Steaks!",
-      marker: "images/Kangaroo.png",
+      marker: "images/resize_Kangaroo.png",
       locTime: []
     },
     {
       name: "Overdone",
       description: "Outrageously ostentatious cupcakes",
-      marker: "images/Cupcake.png",
+      marker: "images/resize_Cupcake.png",
       locTime: []
     },
     {
       name: "The Potato Pup",
       description: "Hotdogs topped with crunchy potato chips",
-      marker: "images/Hot_Dog.png",
+      marker: "images/resize_Hot_Dog.png",
       locTime: []
     }
   ]
