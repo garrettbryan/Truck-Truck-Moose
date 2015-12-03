229186135a1fcb009ccaef839971f392342550a9
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-02 22:50:36 -0500

    fix: foodtrucks property update
    property marker change to more descriptive img

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -7,61 +7,61 @@ var aboutMy = {
     {
       name: "Mammoth Meats",
       description: "Grass fed cows cooked up caveman style.",
-      marker: "images/resize_Woolly_Mammoth.png",
+      img: "images/resize_Woolly_Mammoth.png",
       locTime: []
     },
     {
       name: "The Big Cuban",
       description: "Authentic cuban sandwiches now that the embargo is lifted.!",
-      marker: "images/resize_SandwichSU.png",
+      img: "images/resize_SandwichSU.png",
       locTime: []
     },
     {
       name: "The Icee Snowman",
       description: "Snocones! Try the high fructose corn syrup flavor!",
-      marker: "images/resize_Snowman_SU.png",
+      img: "images/resize_Snowman_SU.png",
       locTime: []
     },
     {
       name: "Tail of the Whale",
       description: "Sushi from a food truck - awesome!",
-      marker: "images/resize_Blue_Whale.png",
+      img: "images/resize_Blue_Whale.png",
       locTime: []
     },
     {
       name: "Cicada Poppers",
       description: "Awesome crunch",
-      marker: "images/resize_Cicada.png",
+      img: "images/resize_Cicada.png",
       locTime: []
     },
     {
       name: "The Sailor's Cup",
       description: "Premium Coffee from around the world",
-      marker: "images/resize_Coffee.png",
+      img: "images/resize_Coffee.png",
       locTime: []
     },
     {
       name: "The Hole Enchilada",
       description: "Fusion Gourmet Donuts",
-      marker: "images/resize_Doughnut.png",
+      img: "images/resize_Doughnut.png",
       locTime: []
     },
     {
       name: "Boomerang Hut",
       description: "Kangaroo Steaks!",
-      marker: "images/resize_Kangaroo.png",
+      img: "images/resize_Kangaroo.png",
       locTime: []
     },
     {
       name: "Overdone",
       description: "Outrageously ostentatious cupcakes",
-      marker: "images/resize_Cupcake.png",
+      img: "images/resize_Cupcake.png",
       locTime: []
     },
     {
       name: "The Potato Pup",
       description: "Hotdogs topped with crunchy potato chips",
-      marker: "images/resize_Hot_Dog.png",
+      img: "images/resize_Hot_Dog.png",
       locTime: []
     }
   ]
