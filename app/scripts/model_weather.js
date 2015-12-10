7b1cf5d1b79339a108e3e6b462af005a0409ef82
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-09 22:40:25 -0500

    Feat: investigate weather underground
    Investigate calls to be made to the weather service to be added to
    the map.

---
+++ app/scripts/model_weather.js
@@ -0,0 +1,13 @@
+//http://api.wunderground.com/api/ec12cd13256c67c5/animatedradar/q/MI/Ann_Arbor.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50
+
+//Using lat/lon bounds to determine a box
+//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=42.35&maxlon=-109.311&minlat=39.27&minlon=-114.644&width=600&height=480&newmaps=1
+
+//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=600&height=480&newmaps=1
+
+
+
+// /Using lat/lon bounds, showing time label, transparent background
+//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
+
+//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
