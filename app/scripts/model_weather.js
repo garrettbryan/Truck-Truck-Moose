73d2a817ed356b10949ab057b11d9596b78de5e8
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-10 23:18:45 -0500

    Feat: more experimentation with weather api
    Use the bounds of the map and the dimesions of the inner window to
    get accurately sized weather maps.

--- app/scripts/model_weather.js
+++ app/scripts/model_weather.js
@@ -1,13 +1,26 @@
-//http://api.wunderground.com/api/ec12cd13256c67c5/animatedradar/q/MI/Ann_Arbor.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50
+/*
 
-//Using lat/lon bounds to determine a box
-//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=42.35&maxlon=-109.311&minlat=39.27&minlon=-114.644&width=600&height=480&newmaps=1
-
-//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=600&height=480&newmaps=1
+http://api.wunderground.com/api/ec12cd13256c67c5/animatedradar/q/MI/Ann_Arbor.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50
 
 
 
-// /Using lat/lon bounds, showing time label, transparent background
-//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
 
-//http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
+Using lat/lon bounds to determine a box
+http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=42.35&maxlon=-109.311&minlat=39.27&minlon=-114.644&width=600&height=480&newmaps=1
+
+http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.693&maxlon=-78.589&minlat=35.526&minlon=-78.719&width=600&height=480&newmaps=1
+
+35.6930034910276,-78.58935914606934
+model_foodtruck.js:238 35.52678677214099,-78.71999345393067
+
+
+/Using lat/lon bounds, showing time label, transparent background
+http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
+
+http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.570&maxlon=-78.627&minlat=35.649&minlon=-78.682&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
+
+http://api.wunderground.com/api/ec12cd13256c67c5/radar/image.gif?maxlat=35.693&maxlon=-78.589&minlat=35.526&minlon=-78.719&width=760&height=1191&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1
+
+http://api.wunderground.com/api/ec12cd13256c67c5/animatedradar/image.gif?maxlat=35.693&maxlon=-78.589&minlat=35.526&minlon=-78.719&width=760&height=1191&rainsnow=1&reproj.automerc=1&num=5&delay=50&timelabel=1&timelabel.x=525&timelabel.y=41
+
+*/
