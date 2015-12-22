f9c38dcdd0c773203013585d40cd2e130823b827
Garrett Bryan <garrettdavisbryan@gmail.com>
2015-12-22 00:10:31 -0500

    Feat: styled main title over map.
    added styling to render title on top of map.

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   console.log("doocument.ready")
   //wait fo title page to load before requesting google map.
+  $('body').prepend(HTML);
+
   fiveSecLoad(google.maps.event.addDomListener(window, 'load', initialize));
 });
 
-  $('body').prepend(HTML);
+
 $(function() { /* code here */ });
 /*
 $("#open-menu").click(function(){
