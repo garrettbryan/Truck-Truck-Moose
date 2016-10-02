3505746f82be4826c877f491cba23a8afdc85c95
Garrett Bryan <garrettdavisbryan@gmail.com>
2016-10-02 13:23:46 -0400

    fix: pass ingredients by value using slice
    ingredient list of the ordered item was passed by reference and was
    therefore edit to it were modifying the ingredient list of the menu
    item. This change passes a new array to the ordered item.

--- app/scripts/models/order-model.js
+++ app/scripts/models/order-model.js
@@ -2,6 +2,6 @@
 var Dish = function (menuItem) {
   this.name = ko.observable(menuItem.name);
   this.price = ko.observable(menuItem.price);
-  this.ingredients = ko.observableArray(menuItem.ingredients);
+  this.ingredients = ko.observableArray(menuItem.ingredients.slice(0));
   this.editing = ko.observable(false);
 };
